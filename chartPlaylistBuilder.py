2e1737bda3013a2b0d85f85c7f8b30a49f079fdc
Jake Causon <jake.causon@gmail.com>
2015-08-22 13:52:51 +0100

    Initital commit

---
+++ chartPlaylistBuilder.py
@@ -0,0 +1,120 @@
+from lxml import html
+import time
+import requests
+import spotify
+import sys
+
+class ChartPlaylistBuilder:
+	def __init__(self, playlistURI):
+
+		self.RATE_LIMIT_DELAY = 1		
+
+		self.playlist_uri = playlistURI
+		#'spotify:user:jakepc123:playlist:62IC0Tug3frZ71uPTnCthK'
+
+		username_file = open('spotify_username.txt', 'r')
+		username = username_file.read();
+		username_file.close()
+
+		password_file = open('spotify_password.txt', 'r')
+		password = password_file.read();
+		password_file.close()
+
+		self.session = spotify.Session()
+		self.session.login(username, password)
+
+		while self.session.connection.state != spotify.ConnectionState.LOGGED_IN:
+		    self.session.process_events()
+
+		self.count = 0
+
+	# Scrape the offical charts page and pull out the charts in order
+	def getCharts(self):
+		page = requests.get("http://www.officialcharts.com/charts/")
+		tree = html.fromstring(page.text)
+
+		artists = tree.xpath('//div[@class="artist"]')
+		titles = tree.xpath('//div[@class="title"]')
+
+		songs = []
+
+		for i in range(0,min(len(artists), len(titles))):
+			a = artists[i].getchildren()[0].text
+			t = titles[i].getchildren()[0].text
+			songs.append({'artist':a, 'title':t})
+
+		return songs
+
+	def deleteAllTracks(self, playlist):
+		playlist.remove_tracks(range(0,len(playlist.tracks)))
+		self.session.process_events()
+		while playlist.has_pending_changes:
+			self.session.process_events()
+
+	# Take a song dict and find it's spotify ID
+	def getSpotifyUID(self, song):
+		self.count = self.count+1
+		query = 'artist:"%s" title:"%s"' % (song.get("artist"), song.get("title"))
+		s = self.session.search(query)
+		s.load()
+
+		time.sleep(self.RATE_LIMIT_DELAY)
+
+		if len(s.tracks) > 0:
+			print "     %d Found '%s'"%(self.count, query)
+			return s.tracks[0]
+			
+		# Attempt with just the title
+		query = 'title:"%s"' % (song.get("title"))
+		s = self.session.search(query)
+		s.load()
+		time.sleep(self.RATE_LIMIT_DELAY)
+
+		if len(s.tracks) > 0:
+			print "!    %d Found '%s' by title only"%(self.count, query)
+			return s.tracks[0]
+		else:
+			print "!!!! %d Can't find the song '%s'"%(self.count, query)
+			return None
+
+	def run(self):
+
+		print "Looking up the current charts..."
+
+		songs = self.getCharts()
+
+		print "I have %d songs from the charts"%len(songs)
+		print "Time to get the spotify ids..."
+
+		tracks = map(self.getSpotifyUID, songs)
+		tracks = [x for x in tracks if x is not None]
+
+		print "I have %d spotify IDs"%len(tracks)
+
+		if len(tracks) == 0:
+			print "Didn't find any tracks, exiting with error"
+			sys.exit(1)
+
+		print "\n\n"
+
+		playlist = self.session.get_playlist(self.playlist_uri)
+		playlist.load()
+		self.deleteAllTracks(playlist)
+		playlist.load()
+		print "Now i'm going to add %d tracks to the playlist"%len(tracks)
+		playlist = self.session.get_playlist(self.playlist_uri)
+		playlist.load()
+		playlist.add_tracks(tracks)
+		count = 0
+		self.session.process_events()
+		while playlist.has_pending_changes or count < len(tracks)+10:
+			count = count+1
+			self.session.process_events()
+		print "Done after processing %d events"%count
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "Please supply a playlist URI as the first argument"
+		sys.exit(2)
+	playlistBuilder = ChartPlaylistBuilder(sys.argv[1])
+	playlistBuilder.run()
---
+++ readme.md
@@ -0,0 +1,49 @@
+# Chart Spotify Playlist Builder
+
+## What is it?
+
+Scrapes the UK official charts website for the top 100 songs and builds a spotify playlist with these songs. Created so it was possible to listen to the charts without needing to listen to radio 1.
+
+## Usage
+
+First you will need to log into Spotify and sign up for a [libspotify](https://developer.spotify.com/technologies/libspotify/) account and get a key file. **You will need to have a paid account to do this.** Place your key file in the cwd and call it ``spotify_appkey.key''.
+
+Next make two text files called ``spotify_username.txt'' and ``spotify_password.txt'' and place these in the cwd. In these text files you should type your spotify username and password. 
+
+It will have to be in plaintext (sorry about that), feel free to find a more secure way of doing it and make a pull request.
+
+Next you need to make a playlist in the spotify ui and get the new playlist's URI (right click on the playlist in the menu and it should be an option). Again at some point I will make it so it can make it's own playlist but feel free to do it yourself and make a pull request.
+
+Great now you are all set! Time to run the command below:
+```bash
+python chartPlaylistBuilder.py 'spotify:user:username:playlist:XXXXXXXXXXXX'
+'''
+where the argument is the playlist URI you got in the last step. Leave it for a few minutes to sort through all the data (good time to go get a cup of tea) and your all done.
+
+## Why does it take so long? (Sales answer)
+
+The Chart Spotify Playlist Builder does a lot of very complex cross platform analysis of the hundreds high-dimensional data points which are involved in building and analysing the data sets required to successfully create a correct and pleasant musical arrangement. Despite many optimisations this takes some time and even on a modern high performance computing machine may take several minutes.
+
+## Why does it take so long? (Technical/correct answer)
+
+I stuck a 1 second delay between requests to Spotify to ensure the script doesn't get caught up in any rate limits I don't know about. This may be unnecessary feel free to look in the code and reduce/remove this at your own risk.
+
+## Contributions
+
+Feel free to post pull requests. Or don't I don't mind.
+
+## License
+
+Copyright 2015 Jacob Causon
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
